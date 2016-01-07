0a50c5bf5a7bac5a1ab7e2e328f859d25020d0a9
ninformations <niti4max@gmail.com>
2016-01-07 10:37:03 +0000

    yo init UIExample
    Signed-off-by: ninformations <niti4max@gmail.com>

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uiexampleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the uiexampleApp
+ */
+angular.module('uiexampleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uiexampleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the uiexampleApp
+ */
+angular.module('uiexampleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
